d16a905084f37a641d359305b4068dc24e11de88
Ian <typppo@gmail.com>
2015-01-19 11:38:56 -0800

    first pass

---
+++ full3d_loader.js
@@ -0,0 +1,33 @@
+;(function() {
+  'use strict';
+
+  var asterank3d = new Asterank3D({
+    container: document.getElementById('container'),
+    not_supported_callback: function() {
+      $('#loading').hide();
+      $('#not-supported').show();
+    },
+    run_asteroid_query: true,
+    show_dat_gui: true,
+    static_prefix: window.passthrough_vars.offline_mode ? 'static' : '/static'
+  });
+
+  if (window.passthrough_vars.offline_mode) {
+    var $canvi = $('canvas');
+    if ($canvi.length > 1) {
+      // workaround for three.js bug in file:// mode
+      $('canvas:first').remove();
+    }
+  }
+
+  // Other wiring
+
+  $('#hide_sidebar').on('click', function() {
+    $('#sidebar').hide();
+    $('#show_sidebar_container').show();
+  });
+  $('#show_sidebar').on('click', function() {
+    $('#sidebar').show();
+    $('#show_sidebar_container').hide();
+  });
+})();
