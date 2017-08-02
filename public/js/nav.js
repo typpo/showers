8e2da16477d7e490216d4f660323352f80a999f0
Ian Webster <ianw_github@ianww.com>
2017-08-01 17:35:15 -0700

    track page view on pushstate navigation

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,5 +1,15 @@
 function navigateTo(view) {
+  var page = '/meteors/' + view.replace(/ /g, '-');
   history.replaceState({view: view},
                        'Meteor showers from space - ' + view,
-                       '/meteors/' + view.replace(/ /g, '-'));
+                       page);
+
+  if (typeof mixpanel !== 'undefined') {
+    mixpanel.track('meteors', {
+      view: view
+    });
+  }
+  if (typeof ga !== 'undefined') {
+    ga('send', 'pageview', '/' + page);
+  }
 }
