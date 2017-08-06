e0c64b2194a422f7fc69b6b3b9dcfd7f00bf642e
Ian Webster <typppo@gmail.com>
2017-08-06 16:24:38 -0700

    export node env production

--- upstart/upstart.sh
+++ upstart/upstart.sh
@@ -7,7 +7,7 @@ mkdir -p /var/log/showers
 echo "starting @ `date`"
 
 # node
-NODE_ENV=production
+export NODE_ENV=production
 supervisor showers.js 2>> /var/log/showers/node.err.log 1>> /var/log/showers/node.out.log &
 
 for job in `jobs -p`
