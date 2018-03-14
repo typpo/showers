988cb462d5de110c2fe8b0325a06fb26d953323e
Ian Webster <ianw_git@ianww.com>
2018-03-13 20:23:26 -0700

    adjust object size

--- public/js/asterank3d.js
+++ public/js/asterank3d.js
@@ -300,7 +300,7 @@
           color: 0xaaaaaa,
           display_color: display_color,
           width: 2,
-          object_size: 20,
+          object_size: 15,
           jed: jed,
           particle_geometry: particle_system_geometry // will add itself to this geometry
         });
